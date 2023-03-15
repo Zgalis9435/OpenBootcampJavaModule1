d401ba5caa35348524acd1adad4e9fc78d5e8f11
Zgalis9435 <zharos.delgado@proton.me>
2023-03-15 22:36:09 +0100

    OpenBootcamp first activity

---
+++ src/datos/tipos/Datos.java
@@ -0,0 +1,36 @@
+package datos.tipos;
+
+
+public class Datos {
+    /**
+     * Introducción a Java
+     * Tipos de datos e imprimirlos en consola
+     */
+    public static void main(String[] args) {
+
+        /*Númericos*/
+        byte variable1=8;
+        int variable2=10;
+        double variable3=12d;
+        float variable4=18.10f;
+        long variable5=178546L;
+        /*Booleanos*/
+        boolean verdad=true;
+        boolean mentira=false;
+        /*Texto*/
+        char letra='a';
+        String texto="3 pelos tiene mi barba";
+
+        System.out.println(variable1);
+        System.out.println(variable2);
+        System.out.println(variable3);
+        System.out.println(variable4);
+        System.out.println(variable5);
+        System.out.println(verdad);
+        System.out.println(mentira);
+        System.out.println(letra);
+        System.out.println(texto);
+
+
+    }
+}
